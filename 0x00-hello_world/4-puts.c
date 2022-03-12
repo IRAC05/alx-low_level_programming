bc5472b84b9801f14840d09deee1081c1e739183
Airat <airatshitta@gmail.com>
2022-03-12 02:39:30 -0800

    update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: 0 if exited properly, non zero otherwise
- *
- int main (void)
- {
-
-      puts ("\"Programming is like building a multilingual puzzle");
-      return (0);
-
- }
+* main - Entry point 
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
